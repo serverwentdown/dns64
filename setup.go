e2baa86755d8167328d2858bf4d8a7fd45b1e099
Ben Kochie <superq@gmail.com>
2019-06-05 10:49:09 +0200

    Update location of CoreDNS proxy plugin
    * Use the new external plugin location.
    * `go fmt`.
    * Update readme.
    
    Signed-off-by: Ben Kochie <superq@gmail.com>

--- setup.go
+++ setup.go
@@ -3,11 +3,12 @@ package dns64
 import (
 	"net"
 	"strconv"
+
 	"github.com/coredns/coredns/core/dnsserver"
 	"github.com/coredns/coredns/plugin"
-	"github.com/coredns/coredns/plugin/pkg/parse"
 	clog "github.com/coredns/coredns/plugin/pkg/log"
-	"github.com/coredns/coredns/plugin/proxy"
+	"github.com/coredns/coredns/plugin/pkg/parse"
+	"github.com/coredns/proxy"
 
 	"github.com/mholt/caddy"
 )
